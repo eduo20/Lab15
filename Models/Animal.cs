a1b1d92660a6d965a29745bdd918b4215b49f4dd
hp <hp@DESKTOP-LU4K9VA>
2026-05-14 09:21:01 -0600

    Agregar archivos de proyecto.

---
+++ Models/Animal.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab15.Models
+{
+    public class Animal
+    {
+        public string nombreCompleto { get; set; } = string.Empty;
+        public string especie { get; set; } = string.Empty;
+        public string tipoAlimentacion { get; set; } = string.Empty;
+        public string tiempoMaxvide { get; set; } = string.Empty;
+
+    }
+}
---
+++ Models/Aves.cs
@@ -0,0 +1,8 @@
+﻿namespace Lab15.Models
+{
+    public class Aves: Animal
+    {
+        public bool vuela { get; set; } = true;
+        public string periodoIncubacion { get; set; } = string.Empty;
+    }
+}
